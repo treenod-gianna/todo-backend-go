b57a0a736fb7902651fe2dbfc5d627dd4fd8fa47
treenod-elly <suhyeon@treenod.com>
2022-11-04 14:11:15 +0900

    :sparkles: get 라우터 추가

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,5 +14,21 @@ func main() {
 			"message": "pong",
 		})
 	})
+
+	todos := []string{"todo1", "todo2", "todo3"}
+
+	r.GET("/todos", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"todos": todos,
+		})
+	})
+	r.GET("/todos/:id", func(c *gin.Context) {
+		id, _ := strconv.Atoi(c.Param("id"))
+		todo := todos[id]
+		c.JSON(http.StatusOK, gin.H{
+			"todo": todo,
+		})
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
