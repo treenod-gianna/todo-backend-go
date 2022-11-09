3ad81bc71f161876bc5eabb0ca073ead14046483
treenod-elly <suhyeon@treenod.com>
2022-11-09 15:50:44 +0900

    :card_file_box: GORM 적용

--- src/main.go
+++ src/main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"gorm.io/gorm"
 	"net/http"
 	"todo-backend-go/src/controllers/todoCtrl"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
 )
 
+type Todo struct {
+	Id        int
+	Title     string
+	Completed bool
+	UserId    int
+}
+
 func main() {
+	dsn := ""
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	var todo *Todo
+	db.First(&todo, "id = ?", "1")
+	fmt.Println(todo)
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
